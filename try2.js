06b60881dd0fc9c73d394605ff3df4cd8a34f034
harumhl <haru.mhl@tamu.edu>
2016-03-08 16:37:23 -0600

    zombies move around on canvas

--- try2.js
+++ try2.js
@@ -4,8 +4,9 @@
 var money = 1000;
 var zombies = [];
 var towers = [];
-var x_positions = [1, 2, 3, 4, 5, 6, 7];
-var y_positions = [2, 3, 4, 5, 6, 7, 8];
+var x_positions = [60,90,120,150,180,210,240,270,300,330,360,390,420, 420,420,420,420,420,420,420];
+var y_positions = [40,40,40,40,40,40,40,40,40,40,40,40,40, 70,100,130,160,190,220,250];
+var myGameArea;
 
 // Object constructor function
 function Zombie(type, health, speed, position_x, position_y, position_index) {
@@ -28,11 +29,45 @@ function Tower(type, health, attack_speed, position_x, position_y) {
     this.position_y = position_y;
     
 }
+myGameArea = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = 480;
+        this.canvas.height = 270;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        //this.frameNo = 0;
+        //this.interval = setInterval(updateGameArea, 20);
+        
+        //this.context.fillStyle = "yellow";
+        //this.context.fillRect(30,30,30,30);
+    },
+    clear : function() {
+        this.context.clearRect(0,0,480,270);
+    },
+    moveWhere : function(x, y, type) {
+        if      (type == "standard")    this.context.fillStyle = "blue";
+        else if (type == "strong")      this.context.fillStyle = "green";
+        else                            this.context.fillStyle = "red";
+        
+        if (x == undefined || y == undefined) return;
+        this.context.fillRect((x||150),(y||30),30,30);
+    },
+    addZombie : function(type) {
+        if      (type == "standard")    this.context.fillStyle = "blue";
+        else if (type == "strong")      this.context.fillStyle = "green";
+        else                            this.context.fillStyle = "red";
+        
+        this.context.fillRect(30,30,30,30);
+    }
+}
+
 function addStandardZombie() {
     if (money < 100) {
         window.alert("Not Enough Money");
         return;
     }
+    myGameArea.addZombie("standard");
     zombies.push({
                  type: "standard",
                  health: 100,
@@ -50,6 +85,7 @@ function addStrongZombie() {
         window.alert("Not Enough Money");
         return;
     }
+    myGameArea.addZombie("strong");
     zombies.push({
                  type: "strong",
                  health: 300,
@@ -70,25 +106,27 @@ function start() {
 }
 function updatePositions() {
     var position_str = "";
+    myGameArea.clear();
+    //    this.context.fillRect(150,30,30,30);
+    
     for (var i=0; i < zombies.length; i++) {
         var pos_index = zombies[i].position_index;
         zombies[i].position_x = x_positions[pos_index+1];
         zombies[i].position_y = y_positions[pos_index+1];
         zombies[i].position_index++;
+        myGameArea.moveWhere(zombies[i].position_x, zombies[i].position_y, zombies[i].type);
         position_str += " [" +zombies[i].position_x + ", " + zombies[i].position_y +"]";
     }
     document.getElementById("positions").innerHTML = "positions: " +position_str;
 }
 function addTower() {
     document.getElementById("towers").innerHTML = "accept input: ";
-    var tower_x = document.getElementById("tower_x").value;
-    var tower_y = document.getElementById("tower_y").value;
+    // "x || 0" just means "if there is a value for x, use that. Otherwise use 0."
+    var tower_x = document.getElementById("tower_x").value || 0;
+    var tower_y = document.getElementById("tower_y").value || 0;
     
-    // default initialization if not provided
-    if (tower_x == "") tower_x = 0;
-    if (tower_y == "") tower_y = 0;
     
-    document.getElementById("towers").innerHTML = "accept input x: [" + tower_x +", " +tower_y +"]";
+    document.getElementById("towers").innerHTML = "accept input x: [" +tower_x +", " +tower_y +"]";
     towers.push({
                 type: "regular",
                 health: 150,
@@ -101,8 +139,31 @@ function addTower() {
         position_str += " [" +towers[i].position_x + ", " + towers[i].position_y +"]";
     }
     document.getElementById("towers").innerHTML = "num: "+towers.length +" towers: " +position_str;
-//    document.getElementById("towers").innerHTML = "tower x: [" + tower_x +", " +tower_y +"] / num: "+towers.length;
 }
+
+var myGamePiece;
+/*
+var a_canvas = document.getElementById("canv");
+var context = canvas.getContext('2d');
+context.rect(100,100,300,300);
+context.fillStyle = 'yellow';
+context.fill();
+ */
+/*
+ var a_canvas = document.getElementById("canv");
+var context = a_canvas.getContext("2d");
+context.fillStyle = "yellow";
+context.beginPath();
+context.arc(95, 85, 40, 0, 2*Math.PI);
+context.closePath();
+context.fill();
+context.lineWidth = 2;
+context.stroke();
+context.fillStyle = "black";
+
+context.font = "30px Garamond";
+context.fillText("Hello, World!",15,175);
+ */
 /*
  document.getElementById("textbox_1").value='';
  document.getElementById("textbox_2").value='';
