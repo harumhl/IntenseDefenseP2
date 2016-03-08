81e5e1ae1a5eb848864adb03c6431523fb4b62a3
haru-mhl <haru.mhl@tamu.edu>
2016-03-07 18:48:23 -0600

    Update and rename main to main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+// Javascript does not need to be compiled, you can put it between <script> and </script> in a file, 
+// save it as something.html and open it in your browser.
+
+<script>
+
+</script>
