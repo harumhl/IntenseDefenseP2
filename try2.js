b5bfe78534467f0b52a01339f73c9d73e177f9f9
harumhl <haru.mhl@tamu.edu>
2016-03-08 14:38:14 -0600

    setInterval, tower position from input, change zombie positions

--- try2.js
+++ try2.js
@@ -4,16 +4,29 @@
 var money = 1000;
 var zombies = [];
 var towers = [];
+var x_positions = [1, 2, 3, 4, 5, 6, 7];
+var y_positions = [2, 3, 4, 5, 6, 7, 8];
 
 // Object constructor function
-function Zombie(type, health, speed, position) {
+function Zombie(type, health, speed, position_x, position_y, position_index) {
     this.type = type;
     this.health = health;
     this.speed = speed;
-    this.position = position;
+    this.position_x = position_x;
+    this.position_y = position_y;
+    this.position_index = position_index;
+    
     this.damage = function (damages) {
         this.health -= damages;
     };
+}
+function Tower(type, health, attack_speed, position_x, position_y) {
+    this.type = type;
+    this.health = health;
+    this.attach_speed = attach_speed;
+    this.position_x = position_x;
+    this.position_y = position_y;
+    
 }
 function addStandardZombie() {
     if (money < 100) {
@@ -24,11 +37,13 @@ function addStandardZombie() {
                  type: "standard",
                  health: 100,
                  speed: 5,
-                 position: 0
+                 position_x: 0,
+                 position_y: 0,
+                 position_index: 0
                  });
     money -= 100;
     document.getElementById("demo").innerHTML = "Num Zombies: " + zombies.length +
-        "\n" + "Money left: " + money;
+    "\n" + "Money left: " + money;
 }
 function addStrongZombie() {
     if (money < 200) {
@@ -39,12 +54,64 @@ function addStrongZombie() {
                  type: "strong",
                  health: 300,
                  speed: 2,
-                 position: 0
+                 position_x: 0,
+                 position_y: 0,
+                 position_index: 0
                  });
     money -= 200;
     document.getElementById("demo").innerHTML = "Num Zombies: " + zombies.length +
-        "\n" + "Money left: " + money;
+    "\n" + "Money left: " + money;
 }
+
+
+// Updating the zombies' positions every second
+function start() {
+    setInterval(updatePositions, 1000); // 1000 miliseconds = 1 sec
+}
+function updatePositions() {
+    var position_str = "";
+    for (var i=0; i < zombies.length; i++) {
+        var pos_index = zombies[i].position_index;
+        zombies[i].position_x = x_positions[pos_index+1];
+        zombies[i].position_y = y_positions[pos_index+1];
+        zombies[i].position_index++;
+        position_str += " [" +zombies[i].position_x + ", " + zombies[i].position_y +"]";
+    }
+    document.getElementById("positions").innerHTML = "positions: " +position_str;
+}
+function addTower() {
+    document.getElementById("towers").innerHTML = "accept input: ";
+    var tower_x = document.getElementById("tower_x").value;
+    var tower_y = document.getElementById("tower_y").value;
+    document.getElementById("towers").innerHTML = "accept input x: [" + tower_x +", " +tower_y +"]";
+    towers.push({
+                type: "regular",
+                health: 150,
+                attack_speed: 3,
+                position_x: tower_x,
+                position_y: tower_y
+    });
+    var position_str = "";
+    for (var i=0; i < towers.length; i++) {
+        position_str += " [" +towers[i].position_x + ", " + towers[i].position_y +"]";
+    }
+    document.getElementById("towers").innerHTML = "num: "+towers.length +" towers: " +position_str;
+//    document.getElementById("towers").innerHTML = "tower x: [" + tower_x +", " +tower_y +"] / num: "+towers.length;
+}
+/*
+ document.getElementById("textbox_1").value='';
+ document.getElementById("textbox_2").value='';
+ if(document.getElementById("textbox_1").focused){
+ document.getElementById("textbox_1").value=text_to_be_inserted;
+ }
+ function addtext() {
+	var newtext = document.myform.inputtext.value;
+	if (document.myform.placement[1].checked) {
+        document.myform.outputtext.value = "";
+    }
+	document.myform.outputtext.value += newtext;
+ }
+ */
 /*
 var zombie1 = new Zombie("standard", 100, 5, 0); // creating an object
 zombie1.damage(10); // calling the object function
