eb425baabd07ffdcde28bc949e7ead5dd82e4ba6
Brandon Ellis <ellisbrandon20@compute-linux1.cs.tamu.edu>
2016-04-22 15:05:59 -0500

    double checked the end of match results works

--- static/ws_server.js
+++ static/ws_server.js
@@ -20,7 +20,7 @@ var server = http.createServer(function(request, response) {
     response.end();
 });
 
-var portNum = 11006;
+var portNum = 11016;
 server.listen(portNum, function() {
     console.log((new Date()) + 'Intese Defense Server is listening on port '+portNum);
 });
