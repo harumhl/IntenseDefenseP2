9c574d19161cd39bab5f9579f1baf22d9d61207c
Adam Hale <adamant1@compute-linux1.cs.tamu.edu>
2016-04-24 23:17:05 -0500

    end of round adjustments

--- static/js/endGame.js
+++ static/js/endGame.js
@@ -6,6 +6,10 @@ endGameState =
 	},
 	create: function()
 	{
+		
+		game.add.sprite(0,0,'title');
+		
+		
 		var opponentName;
 		if(playerNames['attacker'] == player.username)
 			opponentName = playerNames['defender']
@@ -15,41 +19,41 @@ endGameState =
 			if(endGame > 0){
 				if(player.ID > 0){
 					console.log('You win');
-					var winner = game.add.sprite(100,100,"winner");
-					winner.scale.setTo(2.5);
-					game.add.text(180,125, player.username, { font: "25px Arial", fill: "#595959"});
-					var loser = game.add.sprite(100,400,"loser");
+					var winner = game.add.sprite(0,100,"winner");
+					winner.scale.setTo(2);
+					game.add.text(500,350, player.username, { font: "25px Arial", fill: "#595959"});
+					var loser = game.add.sprite(300,600,"loser");
 					//loser.scale.setTo(1.5);
-					game.add.text(180,425, opponentName, { font: "25px Arial", fill: "#595959"});
+					game.add.text(500,800, opponentName, { font: "25px Arial", fill: "#595959"});
 				}				
 				else{
 					console.log('Opponent wins');
-					var winner = game.add.sprite(100,400,"winner");
+					var winner = game.add.sprite(300,600,"winner");
 					//winner.scale.setTo(1.5);
-					game.add.text(180,425, opponentName, { font: "25px Arial", fill: "#595959"});
-					var loser = game.add.sprite(100,100,"loser");
-					loser.scale.setTo(2.5);
-					game.add.text(180,125, player.username, { font: "25px Arial", fill: "#595959"});
+					game.add.text(500,800, opponentName, { font: "25px Arial", fill: "#595959"});
+					var loser = game.add.sprite(0,100,"loser");
+					loser.scale.setTo(2);
+					game.add.text(500,350, player.username, { font: "25px Arial", fill: "#595959"});
 				}
 			}				
 			else{
-				if(player.ID < 0){
+				if(player.ID > 0){
 					console.log('You win');
-					var winner = game.add.sprite(100,100,"winner");
-					winner.scale.setTo(2.5);
-					game.add.text(180,125, player.username, { font: "25px Arial", fill: "#595959"});
-					var loser = game.add.sprite(100,400,"loser");
+					var winner = game.add.sprite(0,100,"winner");
+					winner.scale.setTo(2);
+					game.add.text(500,350, player.username, { font: "25px Arial", fill: "#595959"});
+					var loser = game.add.sprite(300,600,"loser");
 					//loser.scale.setTo(1.5);
-					game.add.text(180,425, opponentName, { font: "25px Arial", fill: "#595959"});
-				}	
+					game.add.text(500,800, opponentName, { font: "25px Arial", fill: "#595959"});
+				}				
 				else{
 					console.log('Opponent wins');
-					var winner = game.add.sprite(100,400,"winner");
+					var winner = game.add.sprite(300,600,"winner");
 					//winner.scale.setTo(1.5);
-					game.add.text(180,425, opponentName, { font: "25px Arial", fill: "#595959"});
-					var loser = game.add.sprite(100,100,"loser");
-					loser.scale.setTo(2.5);
-					game.add.text(180,125, player.username, { font: "25px Arial", fill: "#595959"});
+					game.add.text(500,800, opponentName, { font: "25px Arial", fill: "#595959"});
+					var loser = game.add.sprite(0,100,"loser");
+					loser.scale.setTo(2);
+					game.add.text(500,350, player.username, { font: "25px Arial", fill: "#595959"});
 				}
 			}
 	},
