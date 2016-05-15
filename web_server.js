b3ab4c60ab8c417df412b4361ce5619a576f7141
harumhl <haru.mhl@gmail.com>
2016-05-15 14:11:00 -0500

    Update web_server.js

--- web_server.js
+++ web_server.js
@@ -3,17 +3,16 @@ var app = express();
 app.use('/static', express.static('static'));
 var path = require('path');
 
-console.log("dirname: " +__dirname);
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
     console.log('sending file '+__dirname+'/withPhaser.html');
-    res.sendFile('/static/withPhaser.html');
+    res.sendFile(__dirname + '/static/index.html');
 });
 
 
 var portNum = 13011;
 app.listen(portNum, function(){
-console.log(__dirname);
+console.log("dirname: " +__dirname);
 console.log('Example app listening on port '+portNum);
 
 });
