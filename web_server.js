5794aa68e83f740c0b9684c47fa8328a546bd100
harumhl <haru.mhl@gmail.com>
2016-05-15 15:58:37 -0500

    Update web_server.js

--- web_server.js
+++ web_server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/'));
 
 
 // viewed at http://localhost:8080
-app.get('/static', function(req, res) {
+app.get('/', function(req, res) {
     console.log('sending file '+__dirname+'/static/index.html');
     res.sendFile(__dirname + '/static/index.html');
 });
