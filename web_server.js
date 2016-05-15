08e931b5df5402f427935b30796f207fc47aac7d
harumhl <haru.mhl@gmail.com>
2016-05-15 18:36:22 -0500

    Update web_server.js

--- web_server.js
+++ web_server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var express = require('express');
 var app = express();
 var path = require('path');
-var port = process.env.PORT || 11011;
+var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/'));
 
