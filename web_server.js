89e621a4a435d364ad80bd7bba58aa5e28576de5
harumhl <haru.mhl@gmail.com>
2016-05-15 15:39:43 -0500

    Update web_server.js

--- web_server.js
+++ web_server.js
@@ -11,7 +11,7 @@ app.use(express.static(__dirname + '/'));
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
     console.log('sending file '+__dirname+'/static/index.html');
-    res.sendFile('/static/index.html');
+    res.sendFile(__dirname + '/static/index.html');
 });
 
 var portNum = 13011;
