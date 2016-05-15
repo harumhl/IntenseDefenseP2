434b2fc9553bd9e43c313e99f961508a694374ac
harumhl <haru.mhl@gmail.com>
2016-05-15 18:22:59 -0500

    Update load.js

--- static/js/load.js
+++ static/js/load.js
@@ -120,7 +120,7 @@ window.onload = function() {
     
   // Create a new WebSocket.
   var host = location.origin.replace(/^http/, 'ws');
-  socket = new WebSocket(host+':11011');
+  socket = new WebSocket(host+':11012');
 //  socket = new WebSocket('ws://compute.cse.tamu.edu:11099', "echo-protocol");
   var lockout = 0;
     
