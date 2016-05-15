29d68159aa625581f0c7db1fd57cc05367257a23
harumhl <haru.mhl@gmail.com>
2016-05-15 13:04:16 -0500

    Update ws_server.js

--- static/ws_server.js
+++ static/ws_server.js
@@ -20,6 +20,12 @@ var server = http.createServer(function(request, response) {
     response.writeHead(404);
     response.end();
 });
+
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
 process.on('SIGTERM', server.close.bind(server))
 
 var portNum = 11099;
