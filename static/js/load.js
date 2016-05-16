482b428f015f84cff28ec90cca1025c861b78a9d
harumhl <haru.mhl@gmail.com>
2016-05-15 22:42:03 -0500

    Update load.js

--- static/js/load.js
+++ static/js/load.js
@@ -119,8 +119,6 @@ window.onload = function() {
   // Create a new WebSocket.
   var host = location.origin.replace(/^http/, 'ws');
   console.log("HOST:::" +window.location.port);
-  console.log(location.host);
-  console.log(location.hostname);
 
   socket = new WebSocket(host); // server.listen port number
 //  socket = new WebSocket('ws://compute.cse.tamu.edu:11099', "echo-protocol");
